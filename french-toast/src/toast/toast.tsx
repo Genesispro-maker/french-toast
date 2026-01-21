d6986bfb349b2ae2e61204b588cc57eca81f2740
Genesispro-maker <genesisprog894@gmail.com>
2026-01-21 05:28:59 -0800

    feat(update)

--- french-toast/src/toast/toast.tsx
+++ french-toast/src/toast/toast.tsx
@@ -2,23 +2,25 @@ import styles from "./toast.module.css"
 import { AlertOctagon, AlertTriangle, CheckCircle, Info, XIcon} from "lucide-react";
 
 
-// const ICONS_BY_VARIANT = {
-//   notice: Info,
-//   warning: AlertTriangle,
-//   success: CheckCircle,
-//   error: AlertOctagon,
-// };
+const ICONS_BY_VARIANT = {
+  notice: Info,
+  warning: AlertTriangle,
+  success: CheckCircle,
+  error: AlertOctagon,
+};
 
-export function Toast({variant, children}: {variant: string, children: string}) {
+export function Toast({id, variant, children, handledismiss}: {variant: any, children: string, }) {
+  const Icon = ICONS_BY_VARIANT[variant]
+
   return (
     <div className={`${styles.toast} ${styles[variant]}`}>
       <div className={styles.iconContainer}>
-        <Info size={24} />
+         <Icon size={24}/>
       </div>
       <p className={styles.content}>
         {children}
       </p>
-      <button className={styles.closeButton}>
+      <button className={styles.closeButton} onClick={() => handledismiss(id)}>
         <XIcon size={24} />
       </button>
     </div>
