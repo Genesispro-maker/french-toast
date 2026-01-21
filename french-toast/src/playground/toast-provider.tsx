55df2677aa3c31d8cf62c7a9b5f3ff1abb9c0320
Genesispro-maker <genesisprog894@gmail.com>
2026-01-21 06:06:09 -0800

    Update toast-provider.tsx

--- french-toast/src/playground/toast-provider.tsx
+++ french-toast/src/playground/toast-provider.tsx
@@ -36,7 +36,7 @@ export function ToastProvider({children}){
 
        return (
         <>
-           <ToastContext.Provider value={{toasts, createToast, dismissToast}}>
+           <ToastContext.Provider value={{toasts, createToast, dismissToast, setToasts}}>
                {children}
            </ToastContext.Provider>
         </>
