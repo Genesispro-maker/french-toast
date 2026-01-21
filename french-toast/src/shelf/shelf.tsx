d73ff253f896432c0f5e18979c44ac2270de2af9
Genesispro-maker <genesisprog894@gmail.com>
2026-01-21 06:06:06 -0800

    Update shelf.tsx

--- french-toast/src/shelf/shelf.tsx
+++ french-toast/src/shelf/shelf.tsx
@@ -6,7 +6,7 @@ import { ToastContext } from "../playground/toast-provider";
 export function ToastShelf() {
   const {toasts} = useContext(ToastContext)
   return (
-    <ol className={styles.wrapper}>
+    <ol className={styles.wrapper} role="region" aria-live="polite">
      {toasts.map((toast) => {
       return <li key={toast.id} className={styles.toastWrapper}>
         <Toast id={toast.id} variant={toast.variant}>{toast.message}</Toast>
