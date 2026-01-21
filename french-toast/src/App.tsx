2c53b2bfed943cac4fa92146090e8b99bca63f5e
Genesispro-maker <genesisprog894@gmail.com>
2026-01-21 05:54:05 -0800

    Update App.tsx

--- french-toast/src/App.tsx
+++ french-toast/src/App.tsx
@@ -1,9 +1,12 @@
 import { Playground } from "./playground";
+import { ToastProvider } from "./playground/toast-provider";
 
 export function App(){
   return (
     <>
-      <Playground />
+      <ToastProvider>
+          <Playground />
+      </ToastProvider>
     </>
   )
 }
