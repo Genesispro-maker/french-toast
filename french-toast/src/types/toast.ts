ce15343e5e9311fed4d56cd6808cf6771f013663
Genesispro-maker <genesisprog894@gmail.com>
2026-02-04 05:39:28 -0800

    Update toast.ts

--- french-toast/src/types/toast.ts
+++ french-toast/src/types/toast.ts
@@ -2,12 +2,11 @@ export type Toast = {
   id: string;
   message: string;
   variant: 'notice' | 'warning' | 'success' | 'error';
-  position: "top-right" | "top-left" | "bottom-right" | "bottom-left";
 };
 
 export type ToastContextType = {
   toasts: Toast[];
-  createToast: (message: string, variant: Toast['variant'], position: Toast["position"]) => void;
+  createToast: (message: string, variant: Toast['variant']) => void;
   dismissToast: (id: string) => void;
   setToasts: React.Dispatch<React.SetStateAction<Toast[]>>;
 };
