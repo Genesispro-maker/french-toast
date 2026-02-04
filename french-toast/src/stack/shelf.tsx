23c448dd8188074771d80e76883c85008b8d4366
Genesispro-maker <genesisprog894@gmail.com>
2026-02-04 05:33:22 -0800

    Update shelf.tsx

--- french-toast/src/stack/shelf.tsx
+++ french-toast/src/stack/shelf.tsx
@@ -2,29 +2,22 @@ import { useContext } from "react";
 import { Toast } from "../toast";
 import styles from "./shelf.module.css"
 import { ToastContext } from "../toast-context";
-
 export function ToastShelf() {
+
   const context = useContext(ToastContext)
-  
-    if(!context){
-      throw new Error("ToastContext must be within ToastProvider")
-    }
 
-    const {toasts} = context
+  if(!context){
+    throw new Error("your PAPA")
+  }
 
+  const {toasts} = context
   return (
     <ol className={styles.wrapper} role="region" aria-live="polite">
-      
      {toasts.map((toast) => {
-
-      return <li key={toast.id} className={styles.toastWrapper}>
-
-        <Toast id={toast.id} variant={toast.variant}>{toast.message}</Toast>
-
+      return <li key={toast.id} className={`${styles.toastWrapper} ${styles[toast.position]}`}>
+        <Toast position={toast.position} id={toast.id} variant={toast.variant}>{toast.message}</Toast>
       </li>
-
      })}
-
     </ol>
   );
 }
