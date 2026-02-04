07d93e0a57c8510e774151be6af2f812db57302a
Genesispro-maker <genesisprog894@gmail.com>
2026-02-04 05:33:15 -0800

    Update toast-provider.tsx

--- french-toast/src/playground/toast-provider.tsx
+++ french-toast/src/playground/toast-provider.tsx
@@ -6,11 +6,12 @@ import { ToastContext } from "../toast-context";
 export function ToastProvider({children}: {children: ReactNode}){
      const [toasts, setToasts] = useState<Array<Toast>>([])
 
-    function createToast(message: string, variant: Toast['variant']){
+    function createToast(message: string, variant: Toast['variant'], position: Toast["position"]){
           const nextToasts = [...toasts, {
             id: crypto.randomUUID(),
             message,
             variant,
+            position,
       }]
 
       setToasts(nextToasts)
