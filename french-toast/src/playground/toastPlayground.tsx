3b6663523a4ab8cd0edb540daa86b805d3f25c1b
Genesispro-maker <genesisprog894@gmail.com>
2026-02-04 05:33:17 -0800

    Update toastPlayground.tsx

--- french-toast/src/playground/toastPlayground.tsx
+++ french-toast/src/playground/toastPlayground.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useEffect, useState, type ChangeEvent } from "react";
+import { useCallback, useContext, useEffect, useRef, useState, type ChangeEvent } from "react";
 import styles from "./playground.module.css"
 import { type Toast } from "../types/toast";
 import { ToastShelf } from "../stack";
@@ -7,10 +7,23 @@ import { useEscapeKey } from "../hooks/useEscape";
 
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'] as const;
+const POSITION_OPTIONS = ["top-right", "top-left", "bottom-right" ,"bottom-left"] as const
+
 
 export function Playground(){
     const [message, setMessage] = useState<string>('')
     const [variant, setVariant] = useState<Toast["variant"]>(VARIANT_OPTIONS[0])
+    const [position, setPosition] = useState<Toast["position"]>(POSITION_OPTIONS[0])
+
+    const textAreaRef = useRef<HTMLTextAreaElement | null>(null)
+
+
+   const handleInput = () => {
+    const textarea = textAreaRef.current;
+    if (!textarea) return;
+    textarea.style.height = "auto"; // reset height
+    textarea.style.height = textarea.scrollHeight + "px"; // grow to fit content
+  };
 
 
     const context = useContext(ToastContext)
@@ -25,7 +38,7 @@ export function Playground(){
 
     function handleCreateToast(e: ChangeEvent){
       e.preventDefault()
-       createToast(message, variant)
+       createToast(message, variant, position)
        setMessage('')
        setVariant("notice")
     }
@@ -34,7 +47,7 @@ export function Playground(){
     useEffect(() => {
       function EnterKey(e: KeyboardEvent){
          if(e.key === "Enter"){
-            createToast(message, variant)
+            createToast(message, variant, position)
             setMessage('')
             setVariant("notice")
          }
@@ -45,8 +58,7 @@ export function Playground(){
       return () => {
         window.removeEventListener("keydown", EnterKey)
       }
-    }, [message, variant, createToast])
-    
+    }, [message, variant, createToast, position])
 
    const handleEsc = useCallback(() => {
      setToasts([])
@@ -70,7 +82,7 @@ export function Playground(){
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} value={message} onChange={(e) => setMessage(e.target.value)}/>
+            <textarea ref={textAreaRef} onInput={handleInput} id="message" className={styles.messageInput} value={message} onChange={(e) => setMessage(e.target.value)}/>
           </div>
         </div>
 
@@ -96,13 +108,30 @@ export function Playground(){
             })}
           </div>
         </div>
+       
+       <div className={styles.row}>
+
+        <div className={styles.label}>Position</div>
+
+     <div className={`${styles.PositionWrapper} ${styles.inputWrapper}`}>
+        {POSITION_OPTIONS.map((positions) => {
+           const id = `position-${positions}`;
+          return (
+            <label htmlFor="position">
+              <input id={id} type="radio" name="position" value={positions} checked={positions === position} onChange={(e) => setPosition(e.target.value as Toast["position"])}/>
+              {positions}
+            </label>
+          )
+        })}
+        </div>
+        </div>
 
         <div className={styles.row}>
           <div className={styles.label} />
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <button className={styles.button} onClick={handleCreateToast}>Pop toast</button>
+            <button className={styles.button} onClick={(e) => handleCreateToast(e)}>Pop toast</button>
           </div>
         </div>
       </div>
